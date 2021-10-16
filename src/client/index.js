9a2644f74411217c717d199aec54362c01a67de6
Natalia <petrenkonataly@gmail.com>
2021-10-16 12:00:11 +1100

    Testing

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,14 @@
 import { checkForURL } from './js/urlChecker.js'
 import { handleSubmit } from './js/formHandler.js'
 
-console.log(checkForURL);
+// console.log(checkForURL);
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/main.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
 
 //alert("I EXIST")
 console.log("CHANGE!!");
