3600769f27d2adeb6585377d4f18a5056e215a8f
Natalia <petrenkonataly@gmail.com>
2021-10-27 18:43:24 +1100

    Changed the import path in urlChecker.spec.js

--- __test__/urlChecker.spec.js
+++ __test__/urlChecker.spec.js
@@ -1,5 +1,5 @@
 // Import the js file to test
-import { checkForURL } from "../src/client/js/formHandler";
+import { checkForURL } from "../src/client/js/urlChecker";
 
 describe("Testing the URL validation functionality", () => {
   test("Testing the checkForURL() function", () => {
