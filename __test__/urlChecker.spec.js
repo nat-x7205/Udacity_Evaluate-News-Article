263cfa21c2d7017cb4592bec9b278bcdf9ff9bb2
Natalia <petrenkonataly@gmail.com>
2021-10-27 18:31:01 +1100

    Added Jest testing functionality

---
+++ __test__/urlChecker.spec.js
@@ -0,0 +1,7 @@
+// Import the js file to test
+import { checkForURL } from "../src/client/js/formHandler";
+
+describe("Testing the URL validation functionality", () => {
+  test("Testing the checkForURL() function", () => {
+         expect(checkForURL).toBeDefined();
+})});
